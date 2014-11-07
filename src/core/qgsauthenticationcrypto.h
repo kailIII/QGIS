fa2ef6b5b6e57ed9eeb9d1422ed8d8d7438c2ab8
Larry Shaffer <larrys@dakotacarto.com>
2014-11-07 06:21:07 -0700

    Add license/copyright headers to new source files

--- src/core/qgsauthenticationcrypto.h
+++ src/core/qgsauthenticationcrypto.h
@@ -1,3 +1,19 @@
+/***************************************************************************
+    qgsauthenticationcrypto.h
+    ---------------------
+    begin                : October 5, 2014
+    copyright            : (C) 2014 by Boundless Spatial, Inc. USA
+    author               : Larry Shaffer
+    email                : lshaffer at boundlessgeo dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #ifndef QGSAUTHENTICATIONCRYPTO_H
 #define QGSAUTHENTICATIONCRYPTO_H
 
